06a4c335f5eae502245cc3e314e30d903c19673e
Theego99 <dalca993@gmail.com>
2024-04-16 10:51:35 +0900

    telegram request

--- my-telegram-bot/index.js
+++ my-telegram-bot/index.js
@@ -8,7 +8,7 @@ const PORT = 3000; // You can use any port that is free on your system
 
 // Replace with your actual token and chat ID
 const TELEGRAM_API = `https://api.telegram.org/bot7060741021:AAGSqbOTz3A2vTASIONsNQo0WEqtiwhvKfM/sendMessage`;
-//const CHAT_ID = '5540085604';
+const CHAT_ID2 = '5540085604';
 const CHAT_ID = '7168436504';
 
 app.use(cors()); // This will enable CORS for all routes
@@ -18,17 +18,29 @@ app.post('/send-message', (req, res) => {
     const { name, email, message } = req.body;
     const text = `New message from ${name}, Email: ${email}, Message: ${message}`;
 
-    axios.post(TELEGRAM_API, {
+    // Prepare both requests
+    const requestOne = axios.post(TELEGRAM_API, {
         chat_id: CHAT_ID,
         text: text
-    }).then(response => {
-        console.log('Message sent');
-        res.send('Message sent successfully!');
-    }).catch(error => {
-        console.error('Failed to send message: ', error);
-        res.status(500).send('Failed to send message');
     });
+
+    const requestTwo = axios.post(TELEGRAM_API, {
+        chat_id: CHAT_ID2,
+        text: text
+    });
+
+    // Send both requests in parallel
+    Promise.all([requestOne, requestTwo])
+        .then(responses => {
+            console.log('Messages sent to both chats');
+            res.send('Message sent successfully to both chats!');
+        })
+        .catch(error => {
+            console.error('Failed to send message: ', error);
+            res.status(500).send('Failed to send message');
+        });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
