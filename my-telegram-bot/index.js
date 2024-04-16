dddbc262bd665cdfde0ab4a42a90a8064e9f3d10
Theego99 <dalca993@gmail.com>
2024-04-16 10:12:15 +0900

    tune: tsuyuko telegram

--- my-telegram-bot/index.js
+++ my-telegram-bot/index.js
@@ -8,7 +8,8 @@ const PORT = 3000; // You can use any port that is free on your system
 
 // Replace with your actual token and chat ID
 const TELEGRAM_API = `https://api.telegram.org/bot7060741021:AAGSqbOTz3A2vTASIONsNQo0WEqtiwhvKfM/sendMessage`;
-const CHAT_ID = '5540085604';
+//const CHAT_ID = '5540085604';
+const CHAT_ID = '7168436504';
 
 app.use(cors()); // This will enable CORS for all routes
 app.use(bodyParser.json());
